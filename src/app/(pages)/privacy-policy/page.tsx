54d33fc3d3fcc14840c0e58b05d8cd8fb3622a2b
IzaanAnwar <mdizaan67@gmail.com>
2024-01-28 17:33:29 +0530

    chore: tnc and privacy policy added

---
+++ src/app/(pages)/privacy-policy/page.tsx
@@ -0,0 +1,82 @@
+export default function PrivacyPolicy() {
+  return (
+    <section className="py-16 px-2 md:px-20 lg:px-36 space-y-8">
+      <h1 className="text-5xl text-center font-bold">Privacy Policy</h1>
+      <div className="py-4 space-y-8">
+        <div className="space-y-4">
+          <h5 className="text-xl font-bold">
+            This Privacy Policy describes how ecoachhinna collects, uses, and
+            discloses your Personal Information when you visit or make use of
+            our services.
+          </h5>
+          <p>
+            If you have additional questions, need more information about our
+            privacy practices, or want to make a complaint, please contact us by
+            e-mail at{' '}
+            <a
+              href="mailto:support@ecoachhinna.com"
+              className="hover:text-gray-700 underline"
+            >
+              support@ecoachhinna.com
+            </a>{' '}
+            or by mail using the details provided below:
+          </p>
+        </div>
+        <div className="space-y-4">
+          <h3 className="font-bold text-xl">Collecting Personal Information</h3>
+          <p>
+            When you visit our site, we collect certain information about your
+            device, your interaction with the site, and information necessary to
+            process your purchases. We may also collect additional information
+            if you contact us for customer support. In this Privacy Policy, we
+            refer to any information about an identifiable individual as
+            “Personal Information”. See the list below for more information
+            about what Personal Information we collect and why.
+          </p>
+          <div className="space-y-4">
+            <h5 className="font-bold text-lg">Device information</h5>
+            <p>
+              <span className="font-bold">Purpose of collection:</span> To
+              provide a seamless experience on our site and to perform analytics
+              on site usage to optimize our site.
+              <br />
+              <span className="font-bold">Source of collection:</span> Collected
+              automatically when you access our site using log files, web
+              beacons, tags, or pixels.
+              <br />
+              <span className="font-bold">
+                Disclosure for a business purpose:
+              </span>{' '}
+              Shared with our processor & Google.
+              <br />
+              <span className="font-bold">
+                Personal Information collected:
+              </span>{' '}
+              Version of web browser, IP address, time zone, and how you
+              interact with the site.
+            </p>
+          </div>
+        </div>
+        <div className="space-y-4">
+          <h3 className="font-bold text-xl">Changes</h3>
+          <p>
+            We may update this Privacy Policy from time to time to reflect
+            changes to our practices or for other operational, legal, or
+            regulatory reasons.
+          </p>
+        </div>
+        <div className="space-y-4">
+          <h3 className="font-bold text-xl">Complaints</h3>
+          <p>
+            If you would like to make a complaint, please contact us by e-mail
+            or by mail using the details provided under “Contact” above. If you
+            are not satisfied with our response to your complaint, you have the
+            right to lodge your complaint with the relevant data protection
+            authority. The jurisdiction for all legal complaints and appeals
+            will be at Bombay High Court and District authorities in Mumbai.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
